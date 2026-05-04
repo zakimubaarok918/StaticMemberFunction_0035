4664aef1b9cb489cc8ed96f786b8b80294115270
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-04 11:16:53 +0700

    Implement constructor for mahasiswa class and add main function for object creation

--- constructor.cpp
+++ constructor.cpp
@@ -6,3 +6,11 @@ public:
     mahasiswa(); // Constructor
 };
 
+mahasiswa::mahasiswa() {
+    cout << "Constructor dipanggil!" << endl; // Definisi constructor
+};
+
+int main() {
+    mahasiswa mhs; // Pembuatan Object
+    return 0;
+}
