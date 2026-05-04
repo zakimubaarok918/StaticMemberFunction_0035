14ff8b7fed45a960cf3ad970ab7e8eebebd3de20
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-04 11:31:53 +0700

    Add initial implementation of angka class with constructor and destructor

---
+++ destructor.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+    
+public:
+    angka(int);
+    ~angka();
+    void cetakData():
+    void isiData();    
+};
