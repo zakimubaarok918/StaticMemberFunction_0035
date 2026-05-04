b7dd31eb2163c4b6aedf6ad7cf459499a71a9b1f
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-04 11:35:39 +0700

    Implement constructor and destructor for angka class with data handling

--- destructor.cpp
+++ destructor.cpp
@@ -13,3 +13,18 @@ public:
     void cetakData():
     void isiData();    
 };
+
+angka::angka(int i){
+    panjang = 1;
+    arr = new int[i];
+    isiData();
+}
+angka::~angka(){
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{}
